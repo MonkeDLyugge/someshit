9feca91e513c4a44266fe27378c52222b17f7e77
JimmyNeutron228 <tomkamkad@gmail.com>
2021-10-13 21:06:02 +0300

    work with Thymeleaf, post queries, hibernate repos, filters

---
+++ src/main/java/com/lyugge/pieceofshit/MainController.java
@@ -0,0 +1,44 @@
+package com.lyugge.pieceofshit;
+
+import com.lyugge.pieceofshit.domain.Message;
+import com.lyugge.pieceofshit.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping
+    public String main(Model model) {
+        Iterable<Message> allMessages = messageRepo.findAll();
+        model.addAttribute("allMessages", allMessages);
+        return "index";
+    }
+
+    @PostMapping
+    public String addMessage(@RequestParam String text, @RequestParam String tag, Model model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> allMessages = messageRepo.findAll();
+        model.addAttribute("allMessages", allMessages);
+        return "index";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Model model) {
+        Iterable<Message> messages;
+        if (filter == null || filter.isEmpty()) {
+            messages = messageRepo.findAll();
+        } else {
+            messages = messageRepo.findByTag(filter);
+        }
+        model.addAttribute("allMessages", messages);
+        return "index";
+    }
+}
---
+++ src/main/java/com/lyugge/pieceofshit/repos/MessageRepo.java
@@ -0,0 +1,9 @@
+package com.lyugge.pieceofshit.repos;
+
+import com.lyugge.pieceofshit.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
